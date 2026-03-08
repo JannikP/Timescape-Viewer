eed526d69ce67ca15815878c3e4d64632b95ab1b
Jannik Peters <jannik.peters@yahoo.de>
2026-03-08 09:23:58 +0100

    Added hints to the common scope controls

--- src/views/common.rs
+++ src/views/common.rs
@@ -1,9 +1,11 @@
 use iced::widget::{button, center, column, text};
 use iced::{Element, Length};
+use rust_i18n::t;
 
 use crate::constants::icons::{CLOSE_ICON, COLLAPSE_VERTICAL_ICON, FULLSCREEN_ICON};
 use crate::constants::layout::HANDLE_BAR_WIDTH;
 use crate::messages::Message;
+use crate::widgets::Hint;
 
 pub fn scope_handle_bar<'b>(index: usize) -> Element<'b, Message> {
     column![
@@ -12,14 +14,17 @@ pub fn scope_handle_bar<'b>(index: usize) -> Element<'b, Message> {
             .height(Length::Fill),
         button(COLLAPSE_VERTICAL_ICON) // TODO: Minimize scope button
             .width(HANDLE_BAR_WIDTH)
-            .height(HANDLE_BAR_WIDTH),
+            .height(HANDLE_BAR_WIDTH)
+            .hint(t!("timescape.minimize_scope_hint")),
         button(FULLSCREEN_ICON) // TODO: Maximize scope button
             .width(HANDLE_BAR_WIDTH)
-            .height(HANDLE_BAR_WIDTH),
+            .height(HANDLE_BAR_WIDTH)
+            .hint(t!("timescape.maximize_scope_hint")),
         button(CLOSE_ICON)
             .on_press(Message::RemoveScope(index))
             .width(HANDLE_BAR_WIDTH)
-            .height(HANDLE_BAR_WIDTH),
+            .height(HANDLE_BAR_WIDTH)
+            .hint(t!("timescape.remove_scope_hint")),
     ]
     .width(HANDLE_BAR_WIDTH)
     .height(Length::Fill)
